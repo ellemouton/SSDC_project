ec7a52c7cd2f6dbb919196a439aab77fbad78b5e
Elle Mouton <elle.mouton@gmail.com>
2019-04-06 21:38:24 +0200

    initial commit

---
+++ Software/Transmit/looksea.m
@@ -0,0 +1,28 @@
+% Specify the audio file you want to use:
+fname = '../soundFiles/OceanWaves.wav';
+[y,fs]=audioread(fname);
+
+
+data_fft =fft(y);
+datashift = fftshift(data_fft);
+plot(abs(datashift(:,1)));
+
+% Display the sampling info and length
+sz = size(y);
+n  = sz(1);
+%disp('File: %s\nSamples: %d\nTime   : %d s\n',fname,n,n/fs);
+
+% If longer than 500,000 samples cut if down else things are too slow!
+if (n>500000)
+   n=500000;
+end;
+
+% create time stamps for graph
+x = [0:1/fs:(n-1)/fs];
+
+% Display the graph
+figure; plot(x,y(1:n));
+
+% Play the sea sound
+player = audioplayer(y,fs);
+play(player);
